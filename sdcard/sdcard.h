5b638a5ab441ab966bc4e400cbf097b4d45d05a3
psychotimmy <timholyoake@btopenworld.com>
2024-10-30 18:09:31 +0000

    Add files via upload

---
+++ sdcard/sdcard.h
@@ -0,0 +1,10 @@
+/* SPI pin assignment for Pimoroni Pico VGA base */
+
+#define SDCARD_SPI_BUS     spi1
+#define SDCARD_PIN_SPI0_CS   22
+#define SDCARD_PIN_SPI0_SCK   5
+#define SDCARD_PIN_SPI0_MOSI 18
+#define SDCARD_PIN_SPI0_MISO 19
+
+#define SDCARD_PIO	       pio1
+#define SDCARD_PIO_SM	      0
